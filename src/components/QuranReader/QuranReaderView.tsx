1e82e9340df4e97061d0439075710f24ed909ae7
Muhammed Ameen <ameens.in19@gmail.com>
2025-03-04 11:37:12 +0530

    Update QuranReaderView.tsx

--- src/components/QuranReader/QuranReaderView.tsx
+++ src/components/QuranReader/QuranReaderView.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 import useSyncReadingProgress from './hooks/useSyncReadingProgress';
-import ReadingPreferenceSwitcher from './ReadingPreferenceSwitcher';
 
 import QuranReaderStyles from '@/redux/types/QuranReaderStyles';
 import { VersesResponse } from 'types/ApiResponses';
@@ -13,7 +12,6 @@ import { QuranReaderDataType } from 'types/QuranReader';
 const ReadingView = dynamic(() => import('./ReadingView'));
 
 interface Props {
-  isReadingPreference: boolean;
   quranReaderStyles: QuranReaderStyles;
   quranReaderDataType: QuranReaderDataType;
   initialData: VersesResponse;
@@ -21,33 +19,25 @@ interface Props {
 }
 
 const QuranReaderView: React.FC<Props> = ({
-  isReadingPreference,
   quranReaderStyles,
   quranReaderDataType,
   initialData,
   resourceId,
 }) => {
+  // We'll always use reading mode
+  const isReadingPreference = true;
+
   useSyncReadingProgress({
     isReadingPreference,
   });
 
-  if (isReadingPreference) {
-    return (
-      <>
-        <ReadingView
-          quranReaderStyles={quranReaderStyles}
-          quranReaderDataType={quranReaderDataType}
-          initialData={initialData}
-          resourceId={resourceId}
-        />
-      </>
-    );
-  }
-
   return (
-    <>
-      <ReadingPreferenceSwitcher />
-    </>
+    <ReadingView
+      quranReaderStyles={quranReaderStyles}
+      quranReaderDataType={quranReaderDataType}
+      initialData={initialData}
+      resourceId={resourceId}
+    />
   );
 };
 
